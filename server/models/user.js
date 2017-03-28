c024ddd0a993eec81ccd813eb46d96bbc267e6ab
rizha <rizha.musthafa@gmail.com>
2017-03-28 19:33:33 +0700

    Hash password before saving users

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
   , validator = require('validator')
   , jwt = require('jsonwebtoken')
-  , _ = require('lodash');
+  , _ = require('lodash')
+  , bcrypt = require('bcryptjs');
 
 
 let UserSchema = new mongoose.Schema({
@@ -70,6 +71,21 @@ UserSchema.statics.findByToken = function(token) {
 
 
 };
+
+UserSchema.pre('save', function(next) {
+  let user = this;
+  
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 let User = mongoose.model('User', UserSchema);
 
